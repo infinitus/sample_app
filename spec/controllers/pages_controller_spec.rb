e3809484cfed59e7bc98dae9d95b7f1583f7cd0c
jin <jin@crypt.sg>
2011-08-14 21:10:45 +0800

    Completed Ex. 11.5.2, tests for sidebar micropost counts including pluralization

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,20 +2,49 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
-  
+
   before(:each) do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+
+    describe "for non-signed-in users" do
+
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title", :content => @base_title + " | Home")
+      end
     end
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", :content => @base_title + " | Home") 
+    describe "for signed-in users" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+      end
+
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+
+      it "should show the correct number of microposts" do
+        mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+        get 'home'
+        response.should have_selector("span", :class=>"microposts", :content => @user.microposts.count.to_s)
+      end
+
+      it "should have pluralization on the number of microposts" do
+        mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+        mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+        get 'home'
+        response.should have_selector("span", :class=>"microposts", :content => "2 microposts")
+      end
     end
   end
 
@@ -27,7 +56,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => @base_title + " | Contact") 
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
@@ -39,7 +68,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => @base_title + " | About") 
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 
@@ -51,7 +80,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => @base_title + " | Help") 
+      response.should have_selector("title", :content => @base_title + " | Help")
     end
   end
 
