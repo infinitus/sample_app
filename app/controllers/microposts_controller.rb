812e8cf7336b315c61836d9c0f5cca191aa32b89
jin <jin@crypt.sg>
2011-08-14 22:39:37 +0800

    Completed Ex. 11.5.7, created a nested resource for user > micropost, micropost index action and view

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -18,6 +18,11 @@ class MicropostsController < ApplicationController
     redirect_back_or root_path
   end
 
+  def index
+    @title = "All microposts"
+    @microposts = current_user.microposts.paginate(:page => params[:page])
+  end
+
   private
 
   def authorized_user
